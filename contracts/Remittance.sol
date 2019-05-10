3c291212930223806de8f6822ec39acc67b811be
Andy Bell <andy.bell@barkingmad.io>
2019-05-10 15:34:27 +0200

    Fix security hole, store hash values on chain, withdraw with clear passwords

--- contracts/Remittance.sol
+++ contracts/Remittance.sol
@@ -3,8 +3,8 @@ import "./Running.sol";
 
 contract Remittance is Running
 {
-    bytes32 password1;
-    bytes32 password2;
+    bytes32 public password1;
+    bytes32 public password2;
     bool locked;
 
     event LogDeposit(address owner, bytes32 password1, bytes32 password2, uint256 amount);
@@ -55,15 +55,22 @@ contract Remittance is Running
         emit LogDeposit(getOwner(), password1, password2, msg.value);
     }
 
-    function withdraw(bytes32 _password1, bytes32 _password2)
+    function withdraw(bytes memory _password1, bytes memory _password2)
         public
         isLocked
     {
-        require(_password1 == password1 && _password2 == password2, 'Invalid answer');
+        require(keccak256(_password1) == password1 &&
+                keccak256(_password2) == password2, 'Invalid answer');
+
         uint balance = address(this).balance;
         require(balance > 0, 'No balance available');
         msg.sender.transfer(balance);
 
         emit LogTransfer(getOwner(), msg.sender, password1, password2, balance);
+    }
+
+    function test(bytes memory str1) public pure returns(bytes32)
+    {
+        return keccak256(str1);
     }
 }
